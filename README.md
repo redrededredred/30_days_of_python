b58b8c0ac778092c983e101baf346b8acf03f3ee
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2024-07-24 16:29:01 +0200

    Day 2
    Level 1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 30 Days (or less) of the Snake Language
 
-based on [this](https://github.com/Asabeneh/30-Days-Of-Python)
+based on [this](https://github.com/Asabeneh/30-Days-Of-Python) 
---
+++ day2/main.py
@@ -0,0 +1,25 @@
+"""
+Day 2
+"""
+
+# Level 1
+first_name: str = "John"
+last_name: str = "Hardcore"
+full_name: str = first_name + last_name
+country: str = "Alaska" # :DDDDD
+city: str = "Berlin"
+age: float = 27.3
+year: int = 2024
+is_married: bool = True
+is_true: bool = False
+is_light_on: bool = True
+
+x,y,z = 10  # woah, multi-line variable asignment
+
+# Level 2
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
